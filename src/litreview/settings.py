46a9109fb19f04f690ae8bb760b995ee909a72f6
Brice Venayre <brice.venayre@gmail.com>
2022-09-19 01:07:43 +0200

    update settings: add redirect urls

--- src/litreview/settings.py
+++ src/litreview/settings.py
@@ -125,3 +125,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+LOGIN_REDIRECT_URL = "accounts:login"
+LOGOUT_REDIRECT_URL = "accounts:login"
