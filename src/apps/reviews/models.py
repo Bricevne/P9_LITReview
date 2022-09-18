cfb0d01ad509301e84647f2aecec3aeb44e9e4f7
Brice Venayre <brice.venayre@gmail.com>
2022-09-19 01:27:48 +0200

    update reviews: add Ticket model

--- src/apps/reviews/models.py
+++ src/apps/reviews/models.py
@@ -1,3 +1,11 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Ticket(models.Model):
+    title = models.fields.CharField(max_length=128)
+    description = models.fields.TextField(max_length=2048, blanc=True)
+    image = models.ImageField(null=True, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+
