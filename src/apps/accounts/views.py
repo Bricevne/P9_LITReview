329c833b14d7ffb8bb13d5001f4b63803eb280fd
Brice Venayre <brice.venayre@gmail.com>
2022-09-18 23:07:46 +0200

    update accounts: add SignUpView

--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -1,3 +1,16 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
-# Create your views here.
+from apps.accounts.forms import CustomUserCreationForm
+
+
+class SignUpView(CreateView):
+    """SignUpView class."""
+    form_class = CustomUserCreationForm
+    template_name = "accounts/signup.html"
+    success_url = reverse_lazy("accounts:login")
+
+def login(request):
+    return HttpResponse("<h1>Bienvenue</h1>")
