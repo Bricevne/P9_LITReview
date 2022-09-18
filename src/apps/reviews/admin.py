fa0c2f0d49d88034174e3678a43bb9fe45c2f463
Brice Venayre <brice.venayre@gmail.com>
2022-09-19 01:50:36 +0200

    update reviews: add admin

--- src/apps/reviews/admin.py
+++ src/apps/reviews/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.reviews.models import Ticket, Review, UserFollows
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "time_created",)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("headline", "rating", "ticket", "user",)
+
+
+@admin.register(UserFollows)
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ("user", "followed_user",)
