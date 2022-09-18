8f8718694abce1b2ef6b93e8198ac1ea9e05ab17
Brice Venayre <brice.venayre@gmail.com>
2022-09-18 17:05:45 +0200

    update accounts: add CustomUser model from AbstractUser

--- src/apps/accounts/models.py
+++ src/apps/accounts/models.py
@@ -1,3 +1,8 @@
-from django.db import models
+"""Accounts models."""
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    """Class User."""
+    pass
