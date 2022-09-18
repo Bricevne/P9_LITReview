1df285c572679506d3843ca909b2d95034e0d13d
Brice Venayre <brice.venayre@gmail.com>
2022-09-19 01:09:09 +0200

    update accounts: add profile

--- src/apps/accounts/views.py
+++ src/apps/accounts/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -11,3 +11,8 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     template_name = "accounts/signup.html"
     success_url = reverse_lazy("accounts:login")
+
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
