dcfcf4065657818ccdb740c3a9e587ef37ae9e3f
Brice Venayre <brice.venayre@gmail.com>
2022-09-19 01:08:42 +0200

    update accounts: add login

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.accounts.views import SignUpView
+from apps.accounts.views import SignUpView, profile
 
 app_name = "accounts"
 
 urlpatterns = [
     path("", include('django.contrib.auth.urls')),
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("profile/", profile, name="profile"),
 ]
