2d543bd5a53e420c31b09f4ed82d30b1ed601e82
Brice Venayre <brice.venayre@gmail.com>
2022-09-24 15:21:42 +0200

    update accounts: add verbose name to model fields

--- src/apps/reviews/models.py
+++ src/apps/reviews/models.py
@@ -4,8 +4,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Ticket(models.Model):
-    title = models.fields.CharField(max_length=128)
-    description = models.fields.TextField(max_length=2048, blank=True)
+    title = models.fields.CharField(max_length=128, verbose_name="Titre")
+    description = models.fields.TextField(max_length=2048, blank=True, verbose_name="Description")
     image = models.ImageField(null=True, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
@@ -22,10 +22,11 @@ class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
-        validators=[MinValueValidator(0), MaxValueValidator(5)]
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        verbose_name="Note"
     )
-    headline = models.CharField(max_length=128)
-    body = models.TextField(max_length=8192, blank=True)
+    headline = models.CharField(max_length=128, verbose_name="Titre")
+    body = models.TextField(max_length=8192, blank=True, verbose_name="Commentaire")
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     time_created = models.DateTimeField(auto_now_add=True)
 
