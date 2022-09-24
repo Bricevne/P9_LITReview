7c260f26672c261dd1f563c5a3e428abf5fde2ad
Brice Venayre <brice.venayre@gmail.com>
2022-09-24 12:21:40 +0200

    add ticket detail and update

--- src/apps/reviews/urls.py
+++ src/apps/reviews/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from apps.reviews.views import IndexList, TicketCreate, ReviewCreate
+from apps.reviews.views import IndexList, TicketCreate, ReviewCreate, TicketDetail, TicketUpdate
 
 app_name = "reviews"
 
 urlpatterns = [
     path("", IndexList.as_view(), name="flux"),
     path("tickets/create/", TicketCreate.as_view(), name="ticket-create"),
+    path("tickets/<int:pk>/", TicketDetail.as_view(), name="ticket-detail"),
+    path("tickets/<int:pk>/update", TicketUpdate.as_view(), name="ticket-update"),
     path("tickets/<int:id>/reviews/create/", ReviewCreate.as_view(), name="review-create"),
+    # path("tickets/reviews/create/", review_create, name="ticket-review-create"),
+    # path("tickets/reviews/create/", ReviewNewCreate.as_view(), name="ticket-review-create"),
 ]
--- src/apps/reviews/views.py
+++ src/apps/reviews/views.py
@@ -1,18 +1,20 @@
 from itertools import chain
 
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404
-from django.urls import reverse_lazy
+from django.db import transaction
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DetailView, UpdateView
 
+# from apps.reviews.forms import TicketForm, ReviewForm
 from apps.reviews.models import Ticket, Review
 
 
 @method_decorator(login_required, name='dispatch')
 class IndexList(ListView):
     model = Ticket
-    template_name = "reviews/ticket_list.html"
+    template_name = "reviews/index_list.html"
     context_object_name = "tickets"
 
     def get_context_data(self, **kwargs):
@@ -43,6 +45,22 @@ class TicketCreate(CreateView):
 
 
 @method_decorator(login_required, name='dispatch')
+class TicketDetail(DetailView):
+    model = Ticket
+    context_object_name = "ticket"
+    template_name = "reviews/ticket_detail.html"
+
+
+@method_decorator(login_required, name='dispatch')
+class TicketUpdate(UpdateView):
+    model = Ticket
+    context_object_name = "ticket"
+    fields = ['title', 'description', 'image']
+    template_name = "reviews/ticket_update.html"
+    success_url = reverse_lazy("reviews:flux")
+
+
+@method_decorator(login_required, name='dispatch')
 class ReviewCreate(CreateView):
     model = Review
     template_name = "reviews/review_create.html"
@@ -54,3 +72,31 @@ class ReviewCreate(CreateView):
         ticket = get_object_or_404(Ticket, id=self.kwargs['id'])
         form.instance.ticket = ticket
         return super().form_valid(form)
+
+
+
+
+# @login_required
+# def review_create(request):
+#     ticket_form = TicketForm()
+#     review_form = ReviewForm()
+#     if request.method == 'POST':
+#         if 'create_ticket' in request.POST:
+#             ticket_form = TicketForm()
+#             if ticket_form.is_valid():
+#                 ticket = ticket_form.save(commit=False)
+#                 ticket.user = request.user
+#                 if 'create_review' in request.POST:
+#                     review_form = ReviewForm(request.POST)
+#                     if review_form.is_valid():
+#                         review = review_form.save(commit=False)
+#                         review.user = request.user
+#                         review.ticket = ticket
+#                         ticket.save()
+#                         review.save()
+#                         return reverse('reviews:flux')
+#     context = {
+#         'ticket_form': ticket_form,
+#         'review_form': review_form,
+#     }
+#     return render(request, 'reviews/ticket_review_create.html', context=context)
