c1fecf32e60e9d7441d1a252cf6d72863b651e24
Brice Venayre <brice.venayre@gmail.com>
2022-09-24 12:20:43 +0200

    add templatetags

--- src/apps/reviews/templatetags/reviews_extras.py
+++ src/apps/reviews/templatetags/reviews_extras.py
@@ -1,7 +1,30 @@
 from django import template
+from django.utils import timezone
 
 register = template.Library()
+
+MINUTE = 60
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
 
 @register.filter
 def model_type(instance):
     return type(instance).__name__
+
+
+@register.simple_tag(takes_context=True)
+def get_poster_display(context, user):
+    if user == context['user']:
+        return 'Vous avez'
+    return f"{user.username} a"
+
+
+@register.filter
+def get_posted_at_display(posted_at):
+    seconds_ago = (timezone.now() - posted_at).total_seconds()
+    if seconds_ago <= HOUR:
+        return f'Publié il y a {int(seconds_ago // MINUTE)} minutes.'
+    elif seconds_ago <= DAY:
+        return f'Publié il y a {int(seconds_ago // HOUR)} heures.'
+    return f'Publié le {posted_at.strftime("%m/%d/%Y à %H:%M")}'
+
