6f36f49b9429ad01b9c32801662146f0c9351c04
Brice Venayre <brice.venayre@gmail.com>
2022-09-30 00:32:18 +0200

    add django active-link for nav active items

--- src/litreview/settings.py
+++ src/litreview/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "active_link",
     # My apps
     "apps.accounts",
     "apps.reviews",
