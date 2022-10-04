3eed815f094d17a59f4526915a5bcc7ef3db4863
Brice Venayre <brice.venayre@gmail.com>
2022-10-04 17:53:26 +0200

    add allowed hosts

--- src/litreview/settings.py
+++ src/litreview/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1:8000"]
 
 
 # Application definition
